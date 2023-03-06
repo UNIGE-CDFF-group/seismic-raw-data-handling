5e8756514c866a2f2557c1ea206af1fdaa75d56e
kemichai <mkostas714@gmail.com>
2023-03-06 15:25:59 +0100

    Modify bash commands for example of raw data given here

--- cube2mseed_jackseis.sh
+++ cube2mseed_jackseis.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
-
-#Script to convert from datacube to mseed and change the header
-#requisites: pyrocko
-
-station=temporary #directory name wich contains your data cube inputs
-for st in $station
+# Bash script to convert data format from datacube to mseed and change the header
+# requisites: pyrocko (see readme for installation instructions)
+station_code=PIAN
+path2rawdata='raw_data'
+for st in $path2rawdata
 	do
+	echo $path2rawdata
+	cd $path2rawdata
+	cd $station_code
 
-	echo $station
-	cd $station
-	
-	
 	echo 'converting files to mseed ...'
-
-
-        jackseis *.A* --tinc=86400 --rename-channel='/p0/HHZ/' --rename-channel='/p1/HHN/' --rename-channel='/p2/HHE/' \
-		--rename-network='/.*/DP/' \
-		--output='./convertedTEMP/%n/%s/%n.%s..%c.D.%(wmax_year)s.%j' #set up your output path. If not, the output will go to the directory "temporary" 
-
+        jackseis * --tinc=86400 --rename-channel='/p0/HHZ/' --rename-channel='/p1/HHN/' --rename-channel='/p2/HHE/' \
+		--rename-network='/.*/TP/' \
+		--rename-station='/\d{3}/'$station_code/ \
+		--output='/home/user_name/path2codes/seismic-raw-data-handling/convertedTEMP/%n/%s/%n.%s..%c.D.%(wmax_year)s.%j' #set up your output path. If not, the output will go to the directory "temporary"
 	cd ..
 
 done
+
